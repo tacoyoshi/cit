83563dd39e899674a4faa204e27ea439081219fd
Jeff Easton <JEaston@questaircraft.com>
2019-08-23 14:33:58 -0700

    Modified project to allow multiple serials

--- projects/models.py
+++ projects/models.py
@@ -4,7 +4,7 @@ from serials.models import Serial
 # Create your models here.
 
 class Project(models.Model):
-    serial = models.ForeignKey(Serial, on_delete=models.SET_NULL, blank=True, null=True)
+    serial = models.ManyToManyField(Serial, blank=True)
     project_code = models.CharField(max_length=20, unique=True)
     project_name = models.CharField(max_length=100)
     def __str__(self):
