1f8a535da57d3df2cf1674c388f1cdb0e0ad5314
tacoyoshi <coltyg09@gmail.com>
2019-09-20 14:16:01 -0700

    Station templates working

--- stations/urls.py
+++ stations/urls.py
@@ -5,7 +5,7 @@ app_name = 'stations'
 urlpatterns = [
     path('', views.StationList.as_view(), name='station-list'),
     path('<int:pk>/', views.StationView.as_view(), name='station-detail'),
-    path('station/add/', views.StationCreate.as_view(), name='station-add'),
-    path('station/<int:pk>/', views.StationUpdate.as_view(), name='station-update'),
-    path('station/<int:pk>/delete/', views.StationDelete.as_view(), name='station-delete'),
+    path('add', views.StationCreate.as_view(), name='station-add'),
+    path('<int:pk>/update', views.StationUpdate.as_view(), name='station-update'),
+    path('<int:pk>/delete', views.StationDelete.as_view(), name='station-delete'),
 ]
